30da8a5c0f145dcd068e771e0db19038e7241dcc
admin <dangtinhtong@gmail.com>
2017-11-29 21:59:05 +0700

    first commit

--- Prog6.10/main.m
+++ Prog6.10/main.m
@@ -2,16 +2,27 @@
 //  main.m
 //  Prog6.10
 //
-//  Created by admin on 11/29/17.
-//  Copyright © 2017 admin. All rights reserved.
-//
+//  Created by Tống Đăng Tình on 11/29/17.
+//  Copyright © 2017 Tống Đăng Tình. All rights reserved.
+//Program to generate a table of prime numbers
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        ///sert code here...
+       //NSLog(@"Hello, World!");
+        
+        int p,d,isPrime ;
+        for(p=2;p<=50;++p)
+        {
+            isPrime=1;
+        for(d=2;d<p;++d)
+            if(p%d==0)
+                isPrime=0;
+        if(isPrime !=0 )
+            NSLog(@"%i",p);
+    }
     }
     return 0;
 }
